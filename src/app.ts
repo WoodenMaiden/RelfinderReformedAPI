02114a6d3b0009b7889dc783de1be76173a8482b
Yann P <yann.pomie@laposte.net>
2022-01-24 17:27:29 +0100

    includes queries object

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 require('dotenv').config();
-
+const queries = require('./utils/queriesObject')
 const express = require('express')
+
 const app = express()
-const PORT = process.env.RFR_PORT || 80
+const PORT: number = parseInt(process.env.RFR_PORT, 10) || 80;
 
 app.get("/info", (req: any, res: any) => {
     res.status(200).send({message: "OK!", APIVersion: "1.0.0test"});
 })
-// some important changes here
+
+app.get("/nodes", (req: any, res: any) => {
+    console.log(queries)
+    res.status(200).send(queries)
+})
+
 app.listen(PORT, () => {
-    console.log('\x1b[32m%s\x1b[0m' ,`Server started at port ${PORT}`)
+    console.log('\x1b[32m%s\x1b[0m' ,`Server started at port ${PORT}`);
 })
