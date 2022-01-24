a2f7be4d7cbe00732b7206590fe5e1db5eeb675d
Yann P <yann.pomie@laposte.net>
2022-01-24 14:02:52 +0100

    added simple express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello SPARQL! (and world as well!!)")
+require('dotenv').config();
+
+const express = require('express')
+const app = express()
+const PORT = process.env.RFR_PORT || 80
+
+app.get("/info", (req: any, res: any) => {
+    res.status(200).send({message: "OK!", APIVersion: "1.0.0test"});
+})
+// some important changes here
+app.listen(PORT, () => {
+    console.log('\x1b[32m%s\x1b[0m' ,`Server started at port ${PORT}`)
+})
