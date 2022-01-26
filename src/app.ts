14ab81e1e427fa274bae2f891da3c2da0daefbda
WoodenMAiden <yann.pomie@laposte.net>
2022-01-26 11:39:43 +0100

    made route that fetches all nodes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
+import { Readable } from "stream";
+
 require('dotenv').config();
 const express = require('express')
+
+const sparqlclient = require('./graph/endpoint')
+const queries = require('./graph/queries')
 
 const app = express()
 const PORT: number = parseInt(process.env.RFR_PORT, 10) || 80;
@@ -8,8 +13,15 @@ app.get("/info", (req: any, res: any) => {
     res.status(200).send({message: "OK!", APIVersion: "1.0.0test"});
 })
 
-app.get("/nodes", (req: any, res: any) => {
-    res.status(200).send()
+app.get("/nodes", async (req: any, res: any) => {
+
+    try {
+        const stream: Readable = await sparqlclient.query.select(queries.getAll(), {operation: 'get'})
+        res.status(200).send(stream)
+    }
+    catch (e){
+        res.status(500).send(e)
+    }
 })
 
 app.listen(PORT, () => {
