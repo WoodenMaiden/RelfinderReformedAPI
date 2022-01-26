ffba6ec78cab4bb91ea90dfb7d2f4dc1e075fb2c
WoodenMAiden <yann.pomie@laposte.net>
2022-01-26 11:17:51 +0100

    changed to an abstract class with methods because we could not access attributes

--- src/graph/queries.ts
+++ src/graph/queries.ts
@@ -1,25 +1,14 @@
-interface QueryObject {
-    readonly base: string;
-    readonly prefixes: string;
-//    readonly [key: string]: string;
-    fillQuery (replace: Record<string, string>, query: string): string;
-}
+abstract class Queries /*implements QueryObject*/ {
+
+    static base(): string {return `BASE <http://www.southgreen.fr/agrold/>`};
+
+    static prefixes(): string {return `PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>PREFIX obo:<http://purl.obolibrary.org/obo/>PREFIX vocab:<vocabulary/>`};
+
+    static getAll(): string {return`SELECT ?s ?p ?o {?s ?p ?o.}`};
+    static getAllObjectOf(): string {return `SELECT ?p ?o {!subject! ?p ?o.}`};
 
 
-exports.default = class Queries implements QueryObject {
-
-    base: `BASE <http://www.southgreen.fr/agrold/>`;
-
-    prefixes : `PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX obo:<http://purl.obolibrary.org/obo/>
-    PREFIX vocab:<vocabulary/>`;
-
-    getAll :` SELECT ?s ?p ?o { ?s ?p ?o. }`;
-    getAllObjectOf: `SELECT ?p ?o { !subject! ?p ?o}`;
-
-
-    fillQuery (replace: Record<string, string>, query: string): string {
+    static fillQuery (replace: Record<string, string>, query: string): string {
         let str: string = query
         for (const [key, value] of Object.entries(replace)) {
             str = str.replace(`!${key}!`, value)
@@ -27,3 +16,5 @@ exports.default = class Queries implements QueryObject {
         return str
     };
 }
+
+exports = module.exports = Queries
