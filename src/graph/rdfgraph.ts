e1aea4c3d0fa5ff3eb8d00ec9f45319adab941e8
WoodenMAiden <yann.pomie@laposte.net>
2022-01-27 14:29:27 +0100

    added contruction of inverse graph

--- src/graph/rdfgraph.ts
+++ src/graph/rdfgraph.ts
@@ -1,4 +1,5 @@
 import { MultiDirectedGraph } from "graphology";
+import { Attributes } from "graphology-types";
 
 const Graph = require('graphology').MultiDirectedGraph;
 const queries = require('./queries')
@@ -12,22 +13,32 @@ class RDFGraph {
 
     async init(): Promise<void> {
         const constructedGraph = new MultiDirectedGraph()
+        const graphToInvert = new MultiDirectedGraph()
+
         try {
             const fetchedGraph: Record<string, Record<string, string>>[] = await sparqlclient.query.select(queries.getAll(),{operation: 'get'})
             for (const tuple of fetchedGraph ) {
-                console.log('\x1b[32m%s\x1b[0m', "tuple : ");
-                console.log(tuple);
                 if(!("s" in tuple) || !("p" in tuple) || !("o" in tuple) ) continue;
+
                 if (!constructedGraph.hasNode(tuple.s.value)) constructedGraph.addNode(tuple.s.value)
                 if (!constructedGraph.hasNode(tuple.o.value)) constructedGraph.addNode(tuple.o.value)
-                constructedGraph.addEdgeWithKey(tuple.p.value, tuple.s.value, tuple.o.value)
+                constructedGraph.addDirectedEdge(tuple.s.value, tuple.o.value, {value: tuple.p.value})
             }
+
+            constructedGraph.forEachDirectedEdge((edge: string, attributes: Attributes, source: string, target: string) => {
+                if (!graphToInvert.hasNode(source)) graphToInvert.addNode(source)
+                if (!graphToInvert.hasNode(target)) graphToInvert.addNode(target)
+                graphToInvert.addDirectedEdgeWithKey(edge, target, source, attributes)
+            })
         }
         catch (e) {
             console.log(e)
         }
         finally {
             this.graph = constructedGraph
+            this.invertedGraph = graphToInvert
+            console.log('\x1b[94m%s\x1b[0m', `graph edges = ${constructedGraph.size}, graph nodes = ${constructedGraph.order}`)
+            console.log('\x1b[36m%s\x1b[0m', `inverted graph edges = ${graphToInvert.size}, graph nodes = ${graphToInvert.order}`)
         }
 
     }
