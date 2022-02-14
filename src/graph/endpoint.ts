b37c1be0648da081a5e8482077737e1f82f1a9ed
WoodenMAiden <yann.pomie@laposte.net>
2022-02-14 09:50:40 +0100

    URL depends on env variable SPARQL_ADDRESS

--- src/graph/endpoint.ts
+++ src/graph/endpoint.ts
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const ParsingClient = require('sparql-http-client/ParsingClient')
 
-const endpoint = 'http://sparql.southgreen.fr'
+const endpoint = process.env.SPARQL_ADDRESS
 const client = new ParsingClient({endpointUrl: endpoint})
 
 exports = module.exports = client
