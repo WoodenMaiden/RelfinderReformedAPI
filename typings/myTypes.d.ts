88d8ea1dad31fc8844788abdec78be655d18730a
WoodenMaiden <yann.pomie@etu.umontpellier.fr>
2022-02-17 16:44:56 +0100

    moved types to this file

---
+++ typings/myTypes.d.ts
@@ -0,0 +1,28 @@
+declare module "RFR" {
+	export interface QueryOptions {
+	    excludedClasses: string[],
+	    includedClasses: string[],
+	    excludedNamespaces: string[],
+	    includedNamespaces: string[],
+	    graphs: string[],
+	    offset: number,
+	    limit: number
+	}
+
+	export interface GraphResults {
+	    type: string,
+	    value: string
+	}
+
+	interface EntityResult {
+     type: string,
+     value: string|number|null
+	}
+
+	interface TripleResult {
+     s: EntityResult,
+     p: EntityResult,
+     o: EntityResult
+	}
+	// TODO fix this
+}
