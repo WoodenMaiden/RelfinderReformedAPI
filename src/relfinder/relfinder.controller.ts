fa2aa19d45c1043e47b719d4e0aa1487a1c57ccd
WoodenMaiden <yann.pomie@laposte.net>
2024-01-04 17:58:48 +0100

    feat: rfr endpoint
    Changed from Kosaraju to Tarjan's algorithm
    
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

---
+++ src/relfinder/relfinder.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, HttpCode, Post, Body, Param } from '@nestjs/common';
+
+import { RelFinderService } from './relfinder.service';
+import { RelFinderDTO } from './Relfinder.dto';
+
+@Controller('relfinder')
+export class RelFinderController {
+  constructor(private readonly relFinderService: RelFinderService) {}
+
+  @Post(':depth')
+  @HttpCode(200)
+  async findRelations(
+    @Body() relFinderDTO: RelFinderDTO,
+    @Param('depth') depth: number,
+  ) {
+    
+  }
+}
--- src/sparql/sparql.service.ts
+++ src/sparql/sparql.service.ts
@@ -1,20 +1,33 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 
 import ParsingClient from 'sparql-http-client/ParsingClient';
+import SimpleClient from 'sparql-http-client/SimpleClient';
 import { ResultRow } from 'sparql-http-client/ResultParser';
 
 import { GRAPH_CONFIG, PING } from './constants';
 import { SparqlConfig } from 'src/config/configuration';
 import { measureQueryTime } from 'src/util';
 import { SparqlModule } from './sparql.module';
-import { NodeLabel, Ping } from './sparqlTypes';
+import {
+  NeighborsResult,
+  NodeLabel,
+  Ping,
+  SparqlRawSelect,
+  SparqlRawSelectBinding,
+  TripleResult,
+} from './sparqlTypes';
 
-import { searchForLabel } from './queries';
+import {
+  searchForLabel,
+  getObjectsOf as queryObjectsOf,
+  getGraphUpTo,
+} from './queries';
 import { SearchOptions } from 'src/labels/StoringStrategies';
 
 @Injectable()
 export class SparqlService {
   private parsingClient: ParsingClient;
+  private simpleClient: SimpleClient;
 
   constructor(
     @Inject(GRAPH_CONFIG) private readonly sparqlConfig: SparqlConfig,
@@ -22,6 +35,24 @@ export class SparqlService {
     this.parsingClient = new ParsingClient({
       endpointUrl: sparqlConfig.sparql_address,
     });
+
+    this.simpleClient = new SimpleClient({
+      endpointUrl: sparqlConfig.sparql_address,
+    });
+  }
+
+  private async selectWithoutParsing(query: string): Promise<SparqlRawSelect> {
+    const executedQuery = await measureQueryTime(
+      (await this.simpleClient.query.select(query)).json(),
+    );
+
+    Logger.debug(
+      `Executed SPARQL (unparsed): ${query} in ${executedQuery.time}ms`,
+      SparqlModule.name,
+    );
+    Logger.verbose(executedQuery.result, SparqlModule.name);
+
+    return executedQuery.result;
   }
 
   private async select<Q extends ResultRow>(query: string): Promise<Q[]> {
@@ -30,7 +61,7 @@ export class SparqlService {
     );
 
     Logger.debug(
-      `Executed SPARQL: ${query} in ${executedQuery.time}ms`,
+      `Executed SPARQL (parsed): ${query} in ${executedQuery.time}ms`,
       SparqlModule.name,
     );
     Logger.verbose(executedQuery.result, SparqlModule.name);
@@ -46,5 +77,58 @@ export class SparqlService {
     return this.select<NodeLabel>(
       searchForLabel(text, searchOptions, this.sparqlConfig),
     );
+  }
+
+  async getObjectsOf(subject_url: string) {
+    return this.select<NeighborsResult>(
+      queryObjectsOf(subject_url, this.sparqlConfig),
+    );
+  }
+
+  // This retrieves a subgraph containing all the entities in start_entities up to a depth of maxDepth, then parses it into distinct triples
+  async fetchGraphFrom(
+    start_entities: string[],
+    maxDepth: number,
+  ): Promise<TripleResult[]> {
+    const rawResult = await this.selectWithoutParsing(
+      getGraphUpTo(start_entities, maxDepth, this.sparqlConfig),
+    );
+
+    const order = rawResult.head.vars;
+    const triples = rawResult.results.bindings.flatMap<TripleResult>(
+      (binding: SparqlRawSelectBinding) => {
+        const map = new Map(Object.entries(binding));
+        const variables = order.map<string | undefined>(
+          (variable) => map.get(variable)?.value,
+        );
+
+        let i = 0;
+        const fifo_queue: string[] = [];
+
+        return variables.reduceRight((acc, value) => {
+          ++i;
+
+          if (value) {
+            fifo_queue.push(value);
+          }
+
+          if (i % 3 == 0) {
+            // We should have [object, predicate, subject]
+            // the current subject will be the next object since we read from right to left
+
+            acc.push({
+              o: fifo_queue.shift(),
+              p: fifo_queue.shift(),
+              s: fifo_queue[0],
+            });
+            i = 0;
+          }
+
+          return acc;
+        }, []);
+      },
+    ); // flatMap
+
+    return [...new Set(triples)]; // to remove duplicates
   }
 }
